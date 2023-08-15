be9f7f5d1397c5d831575793ff1875ef0850a225
radian2506 <radiansyah.pro@gmail.com>
2023-08-15 22:14:33 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit similique laborum saepe quisquam impedit repellendus necessitatibus ducimus harum fugiat repudiandae!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
